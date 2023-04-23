25d7b57ebae64cbe8d6af025965585de2cf89c48
Puneeth GH <puneeth4gh@gmail.com>
2023-04-23 02:47:57 -0600

    AR-34 new project creation implemented

--- arxiv-insanity/src/Pages/Dashboard.jsx
+++ arxiv-insanity/src/Pages/Dashboard.jsx
@@ -1,14 +1,15 @@
 import { Fragment } from "react";
-// import React, { useState } from 'react';
-// import axios from "axios";
+import React, { useState } from 'react';
+import axios from "axios";
 
-// import Stack from "@mui/material/Stack";
-// import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
-// import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import TextField from '@mui/material/TextField';
 
 import "./Dashboard.css";
-// import { SearchBar } from "../components/SearchBar";
-// import { SearchResultsList } from "../components/SearchResultsList";
 import SearchData from '../components/ProjectComponent'
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 import Box from '@mui/material/Box';
@@ -21,20 +22,96 @@ import Box from '@mui/material/Box';
 import Header from "../components/Layout/Header"
 import { Button } from "@mui/material";
 
-const onClickNewProject = () => {
-  console.log("onClickNewProject");
-}
 function Dashboard() {
+  const [open, setOpen] = useState(false);
+  const [projectName, setProjectName] = useState("");
+  const [projectDescription, setProjectDescription] = useState("");
+  const [projectTags, setProjectTags] = useState("");
 
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const onClickNewProject = () => {
+    console.log("onClickNewProject");
+    axios({
+      method: "post",
+      url: "http://localhost:8080/api/projects",
+      withCredentials: true,
+      data: {
+        "name": projectName,
+        "description": projectDescription,
+        "tags": projectTags
+      }
+    })
+      .then((response) => {
+        console.log("Created", response);
+        setOpen(false);
+        window.location.reload();
+      })
+      .catch((error) => {
+        console.log("Error Failed", error);
+      })
+  }
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
     <Fragment>
       <Box className="App">
-      <Header />
-      <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2
-       }}>
-          <Button variant="contained" startIcon={<OpenInNewIcon />} size="large" onClick={onClickNewProject}>
+        <Header />
+        <Box sx={{
+          display: 'flex', justifyContent: 'center', mt: 2
+        }}>
+          <Button variant="contained" startIcon={<OpenInNewIcon />} size="large" onClick={handleClickOpen}>
             New Project
           </Button>
+          <Dialog open={open} onClose={handleClose}>
+            <DialogTitle>Create new project</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                To add project, please enter the fields given below.
+              </DialogContentText>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="PEname"
+                label="Project Name"
+                type="text"
+                fullWidth
+                variant="standard"
+                value={projectName}
+                onChange={(e) => setProjectName(e.target.value)}
+              />
+              <TextField
+                autoFocus
+                margin="dense"
+                id="PEdescription"
+                label="Project Description"
+                type="text"
+                fullWidth
+                variant="standard"
+                value={projectDescription}
+                onChange={(e) => setProjectDescription(e.target.value)}
+              />
+              <TextField
+                autoFocus
+                margin="dense"
+                id="PEtags"
+                label="Project tags"
+                type="text"
+                fullWidth
+                variant="standard"
+                value={projectTags}
+                onChange={(e) => setProjectTags(e.target.value)}
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={onClickNewProject}>Create</Button>
+              <Button onClick={handleClose}>Cancel</Button>
+            </DialogActions>
+          </Dialog>
         </Box>
         <SearchData />
       </Box>

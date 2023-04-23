4ad97ecc2e58fc2b93f08a2f641a7f0b8ade725c
Puneeth GH <puneeth4gh@gmail.com>
2023-04-23 02:22:53 -0600

    AR-34 Adding new project button

--- arxiv-insanity/src/Pages/Dashboard.jsx
+++ arxiv-insanity/src/Pages/Dashboard.jsx
@@ -4,13 +4,14 @@ import { Fragment } from "react";
 
 // import Stack from "@mui/material/Stack";
 // import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
-// import Button from "@mui/material/Button";
 // import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 
 import "./Dashboard.css";
 // import { SearchBar } from "../components/SearchBar";
 // import { SearchResultsList } from "../components/SearchResultsList";
 import SearchData from '../components/ProjectComponent'
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+import Box from '@mui/material/Box';
 
 
 // import citationImage from "../assets/citationImage.png";
@@ -18,15 +19,25 @@ import SearchData from '../components/ProjectComponent'
 // import classes from "../components/Layout/Header.module.css";
 
 import Header from "../components/Layout/Header"
+import { Button } from "@mui/material";
 
+const onClickNewProject = () => {
+  console.log("onClickNewProject");
+}
 function Dashboard() {
 
   return (
     <Fragment>
-        <Header />
-      <div className="App">
+      <Box className="App">
+      <Header />
+      <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2
+       }}>
+          <Button variant="contained" startIcon={<OpenInNewIcon />} size="large" onClick={onClickNewProject}>
+            New Project
+          </Button>
+        </Box>
         <SearchData />
-      </div>
+      </Box>
     </Fragment>
   );
 }

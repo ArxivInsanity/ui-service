123e27b54720f095eb63586cba78f10fd89c67eb
ACprime96 <adihyachndrt@gmail.com>
2023-04-29 20:03:11 -0600

    AR-46 Backend integrated

--- arxiv-insanity/src/Pages/MainPage.jsx
+++ arxiv-insanity/src/Pages/MainPage.jsx
@@ -1,22 +1,46 @@
-import React from 'react';
-import Header from '../components/Layout/Header';
-import { useLocation } from 'react-router-dom';
-import { ProjectBreadCrumbs } from '../components/ProjectBreadCrumbs';
+import React, { useState } from "react";
+import Header from "../components/Layout/Header";
+import { useLocation } from "react-router-dom";
+import Grid from "@mui/material/Grid";
+import { ProjectBreadCrumbs } from "../components/ProjectBreadCrumbs";
+import { Box, Paper, Typography } from "@mui/material";
+import { PaperSearchBar } from "../components/PaperSearchBar";
 
 const MainPage = () => {
-    const projectDetails = useLocation().state.data;
-    console.log("STATE: " , projectDetails);
-    return (
-      <>
-        <div className="App">
-          <Header />
-          <ProjectBreadCrumbs
-            projectName={projectDetails.name}
-          ></ProjectBreadCrumbs>
-          {/* {projectDetails.name} */}
-        </div>
-      </>
-    );
-}
+  const [seedPaperId, setSeedPaperId] = useState("");
+  const projectDetails = useLocation().state.data;
+  console.log("STATE: ", projectDetails);
+  return (
+    <>
+      <div className="App">
+        <Header />
+        <ProjectBreadCrumbs
+          projectName={projectDetails.name}
+        ></ProjectBreadCrumbs>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            borderRadius: 0,
+          }}
+        >
+          <Grid sx={{ flexGrow: 1 }} container spacing={1}>
+            <Grid item xs={4}>
+              <Paper sx={{ flexGrow: 1, borderRadius: 0 }}>
+                <PaperSearchBar
+                  seedPaper={seedPaperId}
+                  setSeedPaperFunc={setSeedPaperId}
+                />
+              </Paper>
+            </Grid>
+            <Grid item xs={8}>
+                          <Paper sx={{ flexGrow: 1, borderRadius: 0 }}>{seedPaperId}</Paper>
+            </Grid>
+          </Grid>
+        </Box>
+      </div>
+    </>
+  );
+};
 
 export default MainPage;

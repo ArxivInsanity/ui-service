a9e97c01b29b43aa4d6712c2fde955b67b5768db
MSPawanRanjith <pawanranjith@gmail.com>
2023-04-30 15:37:00 -0600

    AR-42 extracted reading pop up to seperate resuable component

--- src/Components/SeedPaperCard.jsx
+++ src/Components/SeedPaperCard.jsx
@@ -15,11 +15,7 @@ import React, { useCallback, useEffect, useState } from "react";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import { DataGrid } from "@mui/x-data-grid";
 import VisibilityIcon from "@mui/icons-material/Visibility";
-import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
-import DialogTitle from "@mui/material/DialogTitle";
+import PaperDetailsModal from "../Components/PaperDetailsModal"
 import axiosConfig from "../Util/AxiosConfig";
 
 const SeedPaperCard = ({ seedPaperDetails }) => {
@@ -243,121 +239,7 @@ const SeedPaperCard = ({ seedPaperDetails }) => {
           </Box>
         </CardContent>
       </Card>
-      <Dialog open={openRead} onClose={handleClose}>
-        <DialogTitle
-          sx={{
-            fontSize: 20,
-            fontWeight: 700,
-            bgcolor: "#8a2b06",
-          }}
-          color="#ffffff"
-        >
-          {refData?.title}
-          <br />
-          <Chip
-            label={refData?.year}
-            size="medium"
-            sx={{
-              mt: 1,
-              fontSize: 16,
-              color: "#ffffff",
-              border: 1,
-              boxShadow: 10,
-            }}
-            variant="outlined"
-          />
-        </DialogTitle>
-        <Box sx={{ background: "#8a2b06" }}>
-          <DialogContent
-            sx={{ m: 1, borderRadius: 1, boxShadow: 2, background: "#ffffff" }}
-          >
-            <Stack
-              direction="row"
-              sx={{
-                display: "flex",
-                justifyContent: "space-between",
-              }}
-            >
-              <Typography
-                sx={{
-                  fontSize: 18,
-                  fontWeight: 600,
-                }}
-                color="#8a2b06"
-              >
-                Authors :
-              </Typography>
-            </Stack>
-            <Grid
-              sx={{ m: 1, maxHeight: 80, overflowY: "scroll" }}
-              container
-              spacing={1}
-            >
-              {refData?.authors
-                ?.map((authorName) => authorName.name)
-                .map((authorName, id) => {
-                  return (
-                    <Grid item xs={4} key={id + "authorGrid"}>
-                      <Chip
-                        icon={<AccountCircleIcon fontSize="small" />}
-                        label={authorName}
-                        size="medium"
-                      />
-                    </Grid>
-                  );
-                })}
-            </Grid>
-            <Divider sx={{ mt: 2, mb: 2 }} />
-            <Stack
-              direction="row"
-              sx={{
-                display: "flex",
-                justifyContent: "space-between",
-              }}
-            >
-              <Typography
-                sx={{
-                  fontSize: 18,
-                  fontWeight: 600,
-                }}
-                color="#8a2b06"
-              >
-                Abstract :
-              </Typography>
-            </Stack>
-            <DialogContentText
-              sx={{
-                m: 1,
-                fontSize: 16,
-                maxHeight: 200,
-                overflowY: "scroll",
-
-                align: "justify",
-              }}
-              color="text.secondary"
-              variant="body2"
-            >
-              <Box sx={{ m: 1 }}>{refData?.abstract}</Box>
-            </DialogContentText>
-          </DialogContent>
-        </Box>
-        <DialogActions sx={{ justifyContent: "center", bgcolor: "#8a2b06" }}>
-          <Button
-            onClick={() => window.open(refData?.url, "_blank", "noreferrer")}
-            variant="outlined"
-            size="medium"
-            sx={{
-              fontSize: 16,
-              color: "#ffffff",
-              border: 1,
-              boxShadow: 10,
-            }}
-            startIcon={<OpenInNewIcon />}
-          >
-            Read More
-          </Button>
-        </DialogActions>
-      </Dialog>
+      <PaperDetailsModal openRead={openRead} handleClose={handleClose} refData={refData}/>
     </>
   );
 };

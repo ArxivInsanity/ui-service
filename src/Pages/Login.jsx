a64b9d90eddeaea3e67c14c50908243dad03f002
MSPawanRanjith <pawanranjith@gmail.com>
2023-05-01 02:09:16 -0600

    AR-42 changed front login page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,40 +1,157 @@
-import { Fragment } from "react";
+import { Fragment, useCallback } from "react";
 
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import GoogleIcon from "@mui/icons-material/Google";
-
-import citationImage from "../Assets/citationImage.png";
-import treeImage from "../Assets/ourlogo.jpeg";
+import treeImage from "../Assets/arxivinsanitylogo.png";
 import classes from "../Components/Layout/Header.module.css";
-import Header from "../Components/Layout/Header"
+import Header from "../Components/Layout/Header";
 import { getBaseUrl } from "../api/ApiConstants";
+import { loadFull } from "tsparticles";
+import Particles from "react-particles";
 
 const cardStyle = {
-    display: "block",
-    maxWidth: "40%"
-  };
+  display: "block",
+  minWidth: "30%",
+  boxShadow: "3px 3px 3px 3px grey",
+};
 
 const getAuthGoogle = () => {
-    console.log("getAuthGoogle");
-    const ui_base_url = window.location.origin
-    window.open(getBaseUrl() + "/auth/google?redirect_uri=" +ui_base_url + "/auth","_self")
-}
+  console.log("getAuthGoogle");
+  const ui_base_url = window.location.origin;
+  window.open(getBaseUrl() + "/auth/google?redirect_uri=" + ui_base_url + "/auth", "_self");
+};
 
 const Login = (props) => {
+  const particlesInit = useCallback(async (engine) => {
+    console.log(engine);
+    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
+
   return (
     <Fragment>
-        <Header />
-        <div className={classes['main-image']}>
-        <img src = {citationImage} alt='Citation Network'/>
+      <Header />
+      <div style={{ position: "fixed", zIndex: "-1" }}>
+        <Particles
+          style={{ position: "fixed", zIndex: "-1"}}
+          id="tsparticles"
+          init={particlesInit}
+          loaded={particlesLoaded}
+          options={{
+            background: {
+              color: {
+                value: "#ffffff",
+              },
+            },
+            fpsLimit: 120,
+            interactivity: {
+              events: {
+                onClick: {
+                  enable: true,
+                  mode: "push",
+                },
+                onHover: {
+                  enable: true,
+                  mode: "repulse",
+                },
+                resize: true,
+              },
+              modes: {
+                push: {
+                  quantity: 4,
+                },
+                repulse: {
+                  distance: 200,
+                  duration: 0.4,
+                },
+              },
+            },
+            particles: {
+              color: {
+                value: "#8a2b06",
+              },
+              links: {
+                color: "#8a2b06",
+                distance: 150,
+                enable: true,
+                opacity: 0.5,
+                width: 1,
+              },
+              collisions: {
+                enable: true,
+              },
+              move: {
+                directions: "none",
+                enable: true,
+                outModes: {
+                  default: "bounce",
+                },
+                random: false,
+                speed: 3,
+                straight: false,
+              },
+              number: {
+                density: {
+                  enable: true,
+                  area: 800,
+                },
+                value: 80,
+              },
+              opacity: {
+                value: 0.5,
+              },
+              shape: {
+                type: "circle",
+              },
+              size: {
+                value: { min: 1, max: 5 },
+              },
+            },
+            detectRetina: true,
+          }}
+        />
+      </div>
+      <div align="center" className={classes["overlay"]}>
+        <Card style={cardStyle} variant="outlined">
+          <CardMedia
+            sx={{ height: 200, paddingtop: "56.25%", marginTop: "10px", objectFit: "cover" }}
+            image={treeImage}
+          />
+          <CardContent sx={{ marginTop: "10px" }}>
+            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+              Please sign in with Google
+            </Typography>
+          </CardContent>
+          <CardActions style={{ justifyContent: "center", marginBottom: "10px" }}>
+            <Button
+              variant="outlined"
+              sx={{ boxShadow: 7 }}
+              startIcon={<GoogleIcon />}
+              size="large"
+              onClick={getAuthGoogle}
+            >
+              Sign in
+            </Button>
+          </CardActions>
+        </Card>
+      </div>
+      {/* <div className={classes['main-image']}>
+        <img src = {citationImage} alt='Citation Network' style={{height: "100%", overflowY: "hidden"}} />
             <div align="center" className={classes['overlay']}>
                 <Card style={cardStyle} variant="outlined">
                     <CardMedia
-                        sx={{ minHeight: "150px", paddingtop: "56.25%", marginTop:"10px"  }}
+                        sx={{ paddingtop: "56.25%", marginTop:"10px"  }}
                         image={treeImage}
                         title="Arxiv Insanity Card"
                     />
@@ -48,10 +165,10 @@ const Login = (props) => {
                                 Sign in
                             </Button>                
                     </CardActions>
-                </Card>
+                </Card> 
             </div>
             
-        </div>
+  </div> */}
     </Fragment>
   );
 };
